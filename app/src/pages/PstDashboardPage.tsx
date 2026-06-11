2b3e79f66fe99bb8c4e4703e688945fb4e7aa23e
= <bekarysbulat@gmail.com>
2026-06-11 14:48:05 +0500

    Point PST dashboard to latest web app URL

--- app/src/pages/PstDashboardPage.tsx
+++ app/src/pages/PstDashboardPage.tsx
@@ -91,7 +91,7 @@ type ObjectHistoryResponse = {
 
 const DASHBOARD_WEB_APP_URL =
   (import.meta.env.VITE_PST_DASHBOARD_WEB_APP_URL as string | undefined) ||
-  'https://script.google.com/macros/s/AKfycbwSQATuK8XpLDxCcrbm_PEvuzxDovk0jV_JUY09udLrLHd_ILyOOop270MsJCg_S1u_yA/exec';
+  'https://script.google.com/macros/s/AKfycbzv4QxnMmCt7N7TBzeu_h_AWkqvKHNHCGMoaXDOcFIPoEcvEcnmZTzSXekxkg9JpQY7zg/exec';
 const PAGE_SIZE = 25;
 const WEEKLY_PLAN_VALUES = [1300, 1500, 1700, 2000, 2200, 2200, 0];
 
